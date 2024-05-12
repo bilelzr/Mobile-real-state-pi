06b0860c98eed7adac397a41011bd288fd75c531
ski1a <bilelzr8@gmail.com>
2024-05-12 17:52:19 +0100

    add menu to admin view

--- app/src/main/java/com/example/RealEstateApp/Admin/AdminViewPropertyActivity.java
+++ app/src/main/java/com/example/RealEstateApp/Admin/AdminViewPropertyActivity.java
@@ -1,20 +1,28 @@
 package com.example.RealEstateApp.Admin;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.RealEstateApp.AppointmentActivity;
+import com.example.RealEstateApp.ProfileActivity;
 import com.example.RealEstateApp.R;
 import com.example.RealEstateApp.RealStateDatabase;
 import com.example.RealEstateApp.models.Property;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 
 public class AdminViewPropertyActivity extends AppCompatActivity {
 
+    BottomNavigationView bnv;
 
     ListView lv;
     PropertyListAdminAdapter pa;
@@ -30,9 +38,7 @@ public class AdminViewPropertyActivity extends AppCompatActivity {
         db = new RealStateDatabase(this);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Properties");
-        shp_id = getSharedPreferences("Preferences_id", MODE_PRIVATE);
 
-        int user_id = shp_id.getInt("user_id", 0);
 
         ArrayList<Property> properties = new ArrayList<>();
         ArrayList<Property> propertyHouse = db.getAllProperties(RealStateDatabase.TB_HOUSE);
@@ -47,5 +53,33 @@ public class AdminViewPropertyActivity extends AppCompatActivity {
         pa = new PropertyListAdminAdapter(properties, this);
         pa.notifyDataSetChanged();
         lv.setAdapter(pa);
+
+        bnv = findViewById(R.id.bottom_navigation_view_admin);
+        bnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.property:
+                        Intent intent1 = new Intent(getBaseContext(), AdminViewPropertyActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.profile:
+                        Intent intent2 = new Intent(getBaseContext(), ProfileActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.appointments:
+                        Intent intent3 = new Intent(getBaseContext(), AppointmentActivity.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.Benefits:
+                        Intent intent4 = new Intent(getBaseContext(), ProfileActivity.class);
+                        startActivity(intent4);
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 }
